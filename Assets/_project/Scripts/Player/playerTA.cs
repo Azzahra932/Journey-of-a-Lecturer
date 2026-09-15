2badb8014c35c87ea562c2e84d739e3c4a0f62c1
Azzahra932 <amaliaazhara075@gmail.com>
2026-09-15 23:30:06 +0700

    Selesai setup movement dan animasi playerTA

---
+++ Assets/_project/Scripts/Player/playerTA.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class playerTA : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
